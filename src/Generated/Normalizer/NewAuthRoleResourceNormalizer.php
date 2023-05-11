8dce188683852f0a81ea7add7d0287c115d648a3
CircleCI <office@datenkraft.com>
2023-05-11 14:04:42 +0000

    Autogenerated client update [publish]

--- src/Generated/Normalizer/NewAuthRoleResourceNormalizer.php
+++ src/Generated/Normalizer/NewAuthRoleResourceNormalizer.php
@@ -12,7 +12,7 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-class OAuthClientNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+class NewAuthRoleResourceNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
 {
     use DenormalizerAwareTrait;
     use NormalizerAwareTrait;
@@ -20,11 +20,11 @@ class OAuthClientNormalizer implements DenormalizerInterface, NormalizerInterfac
     use ValidatorTrait;
     public function supportsDenormalization($data, $type, $format = null) : bool
     {
-        return $type === 'Datenkraft\\Backbone\\Client\\AuthenticationApi\\Generated\\Model\\OAuthClient';
+        return $type === 'Datenkraft\\Backbone\\Client\\AuthenticationApi\\Generated\\Model\\NewAuthRoleResource';
     }
     public function supportsNormalization($data, $format = null) : bool
     {
-        return is_object($data) && get_class($data) === 'Datenkraft\\Backbone\\Client\\AuthenticationApi\\Generated\\Model\\OAuthClient';
+        return is_object($data) && get_class($data) === 'Datenkraft\\Backbone\\Client\\AuthenticationApi\\Generated\\Model\\NewAuthRoleResource';
     }
     /**
      * @return mixed
@@ -37,29 +37,13 @@ class OAuthClientNormalizer implements DenormalizerInterface, NormalizerInterfac
         if (isset($data['$recursiveRef'])) {
             return new Reference($data['$recursiveRef'], $context['document-origin']);
         }
-        $object = new \Datenkraft\Backbone\Client\AuthenticationApi\Generated\Model\OAuthClient();
+        $object = new \Datenkraft\Backbone\Client\AuthenticationApi\Generated\Model\NewAuthRoleResource();
         if (null === $data || false === \is_array($data)) {
             return $object;
-        }
-        if (\array_key_exists('id', $data)) {
-            $object->setId($data['id']);
-            unset($data['id']);
         }
         if (\array_key_exists('name', $data)) {
             $object->setName($data['name']);
             unset($data['name']);
-        }
-        if (\array_key_exists('identityId', $data)) {
-            $object->setIdentityId($data['identityId']);
-            unset($data['identityId']);
-        }
-        if (\array_key_exists('secret', $data)) {
-            $object->setSecret($data['secret']);
-            unset($data['secret']);
-        }
-        if (\array_key_exists('revoked', $data)) {
-            $object->setRevoked($data['revoked']);
-            unset($data['revoked']);
         }
         foreach ($data as $key => $value) {
             if (preg_match('/.*/', (string) $key)) {
@@ -74,13 +58,7 @@ class OAuthClientNormalizer implements DenormalizerInterface, NormalizerInterfac
     public function normalize($object, $format = null, array $context = array())
     {
         $data = array();
-        $data['id'] = $object->getId();
         $data['name'] = $object->getName();
-        $data['identityId'] = $object->getIdentityId();
-        if ($object->isInitialized('secret') && null !== $object->getSecret()) {
-            $data['secret'] = $object->getSecret();
-        }
-        $data['revoked'] = $object->getRevoked();
         foreach ($object as $key => $value) {
             if (preg_match('/.*/', (string) $key)) {
                 $data[$key] = $value;
