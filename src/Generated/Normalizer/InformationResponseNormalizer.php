2eb805f01cd4b09dd5d96108a23c830ae6a4f387
CircleCI <office@datenkraft.com>
2023-05-23 13:15:14 +0000

    Autogenerated client update [publish]

--- src/Generated/Normalizer/InformationResponseNormalizer.php
+++ src/Generated/Normalizer/InformationResponseNormalizer.php
@@ -44,18 +44,14 @@ class InformationResponseNormalizer implements DenormalizerInterface, Normalizer
         if (\array_key_exists('information', $data)) {
             $values = array();
             foreach ($data['information'] as $value) {
-                $values_1 = new \ArrayObject(array(), \ArrayObject::ARRAY_AS_PROPS);
-                foreach ($value as $key => $value_1) {
-                    $values_1[$key] = $value_1;
-                }
-                $values[] = $values_1;
+                $values[] = $this->denormalizer->denormalize($value, 'Datenkraft\\Backbone\\Client\\AuthenticationApi\\Generated\\Model\\Information', 'json', $context);
             }
             $object->setInformation($values);
             unset($data['information']);
         }
-        foreach ($data as $key_1 => $value_2) {
-            if (preg_match('/.*/', (string) $key_1)) {
-                $object[$key_1] = $value_2;
+        foreach ($data as $key => $value_1) {
+            if (preg_match('/.*/', (string) $key)) {
+                $object[$key] = $value_1;
             }
         }
         return $object;
@@ -69,17 +65,13 @@ class InformationResponseNormalizer implements DenormalizerInterface, Normalizer
         if ($object->isInitialized('information') && null !== $object->getInformation()) {
             $values = array();
             foreach ($object->getInformation() as $value) {
-                $values_1 = array();
-                foreach ($value as $key => $value_1) {
-                    $values_1[$key] = $value_1;
-                }
-                $values[] = $values_1;
+                $values[] = $this->normalizer->normalize($value, 'json', $context);
             }
             $data['information'] = $values;
         }
-        foreach ($object as $key_1 => $value_2) {
-            if (preg_match('/.*/', (string) $key_1)) {
-                $data[$key_1] = $value_2;
+        foreach ($object as $key => $value_1) {
+            if (preg_match('/.*/', (string) $key)) {
+                $data[$key] = $value_1;
             }
         }
         return $data;
